3b1dd60c47f749263f1b5a4bc564dfc2129e9b7f
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-03-04 14:19:04 -0500

    Created Athlete model

--- Models/Athlete.js
+++ Models/Athlete.js
@@ -23,7 +23,7 @@ const AthleteSchema = new mongoose.Schema(
     },
     gender: {
       type: String,
-      enum: ["male", "female", "other"],
+      enum: ["male", "female", "other", ""],
     },
     sports: {
       type: [String],
